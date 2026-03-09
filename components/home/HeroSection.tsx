1622db32ab4e46ba19e3c5daf70cb14b6c601a74
a.d.devnandan <a.d.devnandan@gmail.com>
2026-03-10 03:11:28 +0530

    cleanup and devgov->southlake

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,17 +3,16 @@ import Container from "../ui/Container";
 
 export default function HeroSection() {
   return (
-<section className="relative overflow-hidden min-h-screen flex items-center justify-center text-center">
+<section className="relative overflow-hidden min-h-screen flex  justify-center text-center pt-70">
       <Container className="relative z-10">
-          <h1 className="text-5xl md:text-7xl font-poppins tracking-tight leading-[1.05]">
-            Engineering Without Excuses.
-          </h1>
+          <h1 className="text-5xl md:text-9xl font-poppins font-semibold tracking-tight leading-[1.05]">Southlake          </h1>
 
-        <p className="mt-6 text-lg md:text-xl text-white/60 max-w-2xl mx-auto leading-relaxed">
-          Southlake builds production-grade systems with transparency,
-          governance, and measurable delivery.
-          <br />
-          No chaos. No ghosting. No surprises.
+        <p className="mt-3 text-lg md:text-xl text-white/80 max-w-2xl mx-auto leading-relaxed">
+            Engineering systems that organizations can depend on.
+        </p>
+        <p className="mt-6 text-lg md:text-xl text-white/80 max-w-2xl mx-auto leading-relaxed">
+            Southlake is a software development and research organization focused on building robust, compliant, and scalable systems.
+Our work is grounded in trust, accountability, and long-term commitment.
         </p>
       </Container>
     </section>
