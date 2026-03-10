98e55ffe3ad20f77f2ebcbb409936bbc2a2f191e
a.d.devnandan <a.d.devnandan@gmail.com>
2026-03-11 03:10:25 +0530

    layout change

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/home/HeroSection";
-import WhySection from "@/components/home/WhySection";
+import WhySection from "@/components/home/Projects";
 import WhatWeBuild from "@/components/home/WhatWeBuild";
 import ProofOfProcess from "@/components/home/ProofOfProcess";
 import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
@@ -8,9 +8,8 @@ export default function Home() {
   return (
     <div className="font-poppins">
       <HeroSection />
-      <WhySection />
       <WhatWeBuild />
-      <ProofOfProcess />
+      <WhySection />
     </div>
   );
 }
